ae362938dbd344133ac0230b55c4dbc4c3c3c59b
Oleksandr <boltbright525@gmail.com>
2024-02-10 23:20:38 +0200

    bump

--- src/Config.php
+++ src/Config.php
@@ -6,32 +6,32 @@ class Config
 {
     public static function get(string $key, $default = null): mixed
     {
-        if (!file_exists(__DIR__ . '/config/config.php')) {
+        if (!file_exists(NTH_ROOT . '/../src/Config/config.php')) {
             return null;
         }
 
-        $config = require __DIR__ . '/config/config.php';
+        $config = require NTH_ROOT . '/../src/Config/config.php';
 
         return $config[$key] ?? $default;
     }
 
     public static function getBlock(string $key, $default = null): mixed
     {
-        if (!file_exists(__DIR__ . '/config/blocks.php')) {
+        if (!file_exists(NTH_ROOT . '/../src/Config/blocks.php')) {
             return null;
         }
 
-        $config = require __DIR__ . '/config/blocks.php';
+        $config = require NTH_ROOT . '/../src/Config/blocks.php';
 
         return $config[$key] ?? $config[$default];
     }
 
     public static function getBlocks(): array
     {
-        if (!file_exists(__DIR__ . '/config/blocks.php')) {
+        if (!file_exists(NTH_ROOT . '/../src/Config/blocks.php')) {
             return [];
         }
 
-        return require __DIR__ . '/config/blocks.php';
+        return require NTH_ROOT . '/../src/Config/blocks.php';
     }
 }
