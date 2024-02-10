1cc2aaf5e91f088633c93b202b181573390ca659
Oleksandr <boltbright525@gmail.com>
2024-02-10 23:14:18 +0200

    bump

--- src/Controllers/Controller.php
+++ src/Controllers/Controller.php
@@ -14,53 +14,46 @@ class Controller
     /**
      * Setup guard.
      *
-     * @return Response|null
-     * @throws Exception
+     * @return void
      */
-    public function setupGuard(): ?Response
+    public function setupGuard(): void
     {
         $user = DB::find(User::class, ['role' => 'admin']);
         $site_meta = DB::find(Meta::class, ['meta_name' => 'site_config']);
 
         if (!$user || !$site_meta) {
-            return (new Response)->redirect('/setup/account');
+            (new Response)->redirect('/setup/account')->send();
         }
-
-        return null;
     }
 
     /**
      * Not setup guard.
      *
-     * @return Response|null
-     * @throws Exception
+     * @return void
      */
-    public function notSetupGuard(): ?Response
+    public function notSetupGuard(): void
     {
         $user = DB::find(User::class, ['role' => 'admin']);
         $site_meta = DB::find(Meta::class, ['meta_name' => 'site_config']);
 
         if ($user && $site_meta) {
-            return (new Response)->redirect('/admin');
+            (new Response)->redirect('/admin')->send();
         }
-
-        return null;
     }
 
-    public function authenticatedGuard(): ?Response
+    public function authenticatedGuard(): void
     {
         $auth_token = (new Request)->session()->get('auth_token');
 
         // If there is no auth token, redirect to sign in page.
         if (!$auth_token) {
-            return (new Response)->redirect('/admin/signin');
+            (new Response)->redirect('/admin/signin')->send();
+            return;
         }
 
         // If the user with the auth token does not exist, redirect to sign in page.
         if (!DB::find(User::class, ['auth_token' => $auth_token])) {
-            return (new Response)->redirect('/admin/signin');
+            (new Response)->redirect('/admin/signin')->send();
         }
-
-        return null;
     }
 }
