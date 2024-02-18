30994c863a581efa5ef8b4b36e963cbc8666a574
Oleksandr <boltbright525@gmail.com>
2024-02-18 16:29:21 +0200

    Fixes

--- public/assets/admin/js/utils.js
+++ public/assets/admin/js/utils.js
@@ -1,21 +1,36 @@
+function debounce(f, ms) {
+  let timer;
+
+  return (...args) => {
+    clearTimeout(timer);
+
+    timer = setTimeout(() => {
+      f(...args);
+    }, ms);
+  };
+}
+
 function autogrow(els) {
   els.forEach((el) => {
     const textarea = el.querySelector("textarea");
+    let scrollPos = window.scrollY;
 
     textarea.style.height = `0px`;
     textarea.style.height = `${textarea.scrollHeight}px`;
 
     textarea.addEventListener("input", (_) => {
-      const scrollPos = window.scrollY;
+      scrollPos = window.scrollY;
 
       textarea.style.height = `0px`;
       textarea.style.height = `${textarea.scrollHeight}px`;
-
-      window.scrollTo(0, scrollPos);
-      
-      setTimeout(() => {
-        window.scrollTo(0, scrollPos);
-      }, 50);
     });
+
+    const ro = new ResizeObserver((entries) => {
+      if (scrollPos > 0) {
+        window.scrollTo(0, scrollPos);
+      }
+    });
+
+    ro.observe(textarea);
   });
 }
