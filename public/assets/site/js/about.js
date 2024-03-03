ebfeb2aeba1d656cc003f0f52a981d94b337ce51
Oleksandr <boltbright525@gmail.com>
2024-03-03 22:45:04 +0200

    Bump

--- public/assets/site/js/about.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("load", () => {
-  const toggles = document.querySelectorAll(".toggle-about-section");
-
-  toggles.forEach((el) => {
-    el.addEventListener("click", () => {
-      document.querySelector("body").classList.toggle("js-about-section-open");
-    });
-  });
-});
