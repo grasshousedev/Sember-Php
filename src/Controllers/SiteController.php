4a0786f7fcdee319d92c1c86e891111617fb6e51
Oleksandr <boltbright525@gmail.com>
2024-03-08 10:26:56 +0200

    bump

--- src/Controllers/SiteController.php
+++ src/Controllers/SiteController.php
@@ -5,6 +5,7 @@ namespace Asko\Sember\Controllers;
 use Asko\Sember\Database;
 use Asko\Sember\Models\Meta;
 use Asko\Sember\Models\Post;
+use Asko\Sember\Request;
 use Asko\Sember\Response;
 use Parsedown;
 
@@ -75,7 +76,7 @@ readonly class SiteController
      * @param string $slug
      * @return Response
      */
-    public function post(Response $response, string $slug): Response
+    public function post(Request $request, Response $response, string $slug): Response
     {
         $post = $this->db->findOne(
             model: Post::class,
@@ -90,18 +91,23 @@ readonly class SiteController
         // Increment the views.
         $viewed_cookie_k = "has_read_" . $post->get("id");
 
-        if (!isset($_COOKIE[$viewed_cookie_k])) {
-            setcookie($viewed_cookie_k, "yes", time() + 60 * 60 * 24 * 30);
+        if (!$request->cookie()->has($viewed_cookie_k)) {
+            $request->cookie()->set($viewed_cookie_k, "yes", time() + 60 * 60 * 24 * 30);
             $post->set("views", ($post->get("views") ?? 0) + 1);
             $this->db->update($post);
         }
 
         $post->set("html", $post->renderHtml());
-        $site_title = $this->db->findOne(Meta::class, "where meta_name = ?", [
-            "site_name",
-        ]);
 
-        return $response->view("site/post", [
+        $site_title = $this->db->findOne(
+            model: Meta::class,
+            query: "where meta_name = ?",
+            data: ["site_name"]
+        );
+
+        $post_type = $post->get("type") ?? "post";
+
+        return $response->view("site/{$post_type}", [
             "page_title" => $post->get("title"),
             "post" => $post,
             "site_name" => $site_title->get("meta_value"),
